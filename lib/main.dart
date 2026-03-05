0592818498911e4d1e82889faee47e8680c13d94
khuslendalai <2022140@miu.edu.mn>
2026-03-05 17:07:51 +0800

    after firebase cli isntallation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
-import 'ui/2_widget_layout/widgets_layout_demo.dart';
+import 'package:flutter_demo/services/local_storage/local_storage.dart';
+import 'package:flutter_demo/services/service_locator.dart';
+import 'package:flutter_demo/ui/2_widget_layout/widgets_layout_demo.dart';
+import 'package:flutter_demo/ui/3_state_managment/state_management_demo.dart';
+import 'package:flutter_demo/ui/4_user_login/login_screen.dart';
 import 'ui/1_dart/dart_demo_screen.dart';
-import 'ui/3_state_management/state_management_demo.dart';
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupServiceLocator();
+  await getIt<LocalStorage>().init();
   runApp(const MyApp());
 }
 
@@ -55,8 +62,9 @@ class HomeScreen extends StatelessWidget {
               );
             },
           ),
+
           ListTile(
-            title: const Text("3. State Management"),
+            title: const Text("3. State management"),
             leading: const Icon(Icons.code),
             onTap: () {
               Navigator.push(
@@ -67,9 +75,19 @@ class HomeScreen extends StatelessWidget {
               );
             },
           ),
+
+          ListTile(
+            title: const Text("4. User login"),
+            leading: const Icon(Icons.code),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const LoginScreen()),
+              );
+            },
+          ),
         ],
       ),
     );
   }
 }
-
