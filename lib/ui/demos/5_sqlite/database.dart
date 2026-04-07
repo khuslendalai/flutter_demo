1f7b8f5bda6b0f0b6b801b0710f3790364bce721
khuslendalai <2022140@miu.edu.mn>
2026-04-07 14:07:48 +0800

    pushing as bagshaa said

--- lib/ui/demos/5_sqlite/database.dart
+++ lib/ui/demos/5_sqlite/database.dart
@@ -1,5 +1,8 @@
 
+import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
 
 class DatabaseHelper {
   static const _databaseName = "my_database.db";
@@ -9,10 +12,30 @@ class DatabaseHelper {
   static const columnId = '_id';
   static const name = 'name';
   static const age = 'age';
+
+  late Database _database;
+
   Future<void> init() async {
     final folder = await getApplicationDocumentsDirectory();
-    final path = '$folder/$_databaseName';
+    final path = join(folder.path, _databaseName);
+    _database = await openDatabase(
+      path,
+      version: _databaseVersion,
+      onCreate: (db, version) async {
+        
+      },
+    );
     print(path);
 
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    final sql = '''
+      CREATE TABLE $table (
+        $columnId INTEGER PRIMARY KEY,
+        $name TEXT NOT NULL,
+        $age INTEGER NOT NULL
+      )
+    ''';
   }
 }
