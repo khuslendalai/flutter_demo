13880e174613bbb7984479dafb626bb9970e77fa
khuslendalai <2022140@miu.edu.mn>
2026-05-14 14:50:36 +0800

    permissions

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/app_state.dart';
+import 'package:flutter_demo/services/local_storage/local_storage.dart';
 import 'package:flutter_demo/services/service_locator.dart';
 import 'package:flutter_demo/theme.dart';
+import 'package:flutter_demo/ui/demos/12_permissions/permissions_demo.dart';
 import 'package:flutter_demo/ui/demos/2_widget_layout/widgets_layout_demo.dart';
 import 'package:flutter_demo/ui/demos/3_state_managment/state_management_demo.dart';
 import 'package:flutter_demo/ui/demos/5_sqlite/sqlite_demo.dart';
@@ -9,13 +11,15 @@ import 'package:flutter_demo/ui/demos/6_networking/networking_demo.dart';
 import 'package:flutter_demo/ui/demos/7_testing/calculator_demo.dart';
 import 'package:flutter_demo/ui/demos/8_profiling/profiling_demo.dart';
 import 'package:flutter_demo/ui/demos/9_painting/painting_demo.dart';
-import 'package:flutter_demo/ui/demos/12_permissions/permissions_demo.dart';
 import 'package:flutter_demo/ui/settings/settings_screen.dart';
 import 'ui/demos/1_dart/dart_demo_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupServiceLocator();
+  await getIt<LocalStorage>().init();
+  // TODO: fix firebase
+  // await getIt<Auth>().init();
   await getIt<AppState>().init();
   runApp(const MyApp());
 }
@@ -179,7 +183,9 @@ class HomeScreen extends StatelessWidget {
             onTap: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => const PermissionsDemo()),
+                MaterialPageRoute(
+                  builder: (context) => const PermissionsDemo(),
+                ),
               );
             },
           ),
